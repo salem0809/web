3a6dd9ba746e5437371fcff7f0ac110b852441bb
salimjon <amanovamuhabat1982@gmail.com>
2025-01-21 09:50:33 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+a=int(input("lubbo'y son kirit>>>"))
+b=int(input("libbo'y bitta boshqa son kirgaz>>>"))
+c=a+b
+print(c,"san izlagan son>>>")
--- servis.py
+++ servis.py
@@ -1,12 +1,2 @@
-def signup(username, password):
-    f = open("users.txt", "a")
-    f.write(f"{username} {password}\n")
-    f.close()
-
-    def login(username, password):
-        f = open("users.txt", "r")
-        isTrue = False
-        for line in f:
-            if line == f"{username} {password}\n":
-                isTrue=True
-                break
+a=int(input("lubbo'y son kirit"))
+b=int(input("libbo'y bitta boshqa son kirit"))
